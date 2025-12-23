60c5b7fb2d0091684740d4a41643f3bf19ca8fb9
quochuy1012 <nguyenphamquochuy1012@gmail.com>
2025-12-23 20:00:37 +0700

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,7 @@ const nextConfig = {
   }),
   trailingSlash: false,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
 export default nextConfig
